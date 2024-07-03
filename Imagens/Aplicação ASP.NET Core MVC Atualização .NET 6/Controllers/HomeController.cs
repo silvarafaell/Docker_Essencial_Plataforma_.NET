b8a784fde691ad5e45eb928d366e7529670bba7d
francisco rafael <francisco_rafael@hotmail.com.br>
2024-07-03 07:36:00 -0300

    Aplicação ASP.NET Core MVC - Atualização .NET 6

---
+++ Imagens/Aplicação ASP.NET Core MVC Atualização .NET 6/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc1.Models;
+
+namespace mvc1.Controllers;
+
+public class HomeController : Controller
+{
+    private IRepository repository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private string message;
+    public HomeController(IRepository repo,
+     IHttpContextAccessor httpContextAccessor)
+    {
+        repository = repo;
+        _httpContextAccessor = httpContextAccessor;
+        var hostname = _httpContextAccessor.HttpContext.Request.Host.Value;
+        message = $"Docker - ({hostname})";
+    }
+    public IActionResult Index()
+    {
+        ViewBag.Message = message;
+        return View(repository.Produtos);
+    }
+}
---
+++ Imagens/Aplicação ASP.NET Core MVC Atualização .NET 6/Dockerfile
@@ -0,0 +1,6 @@
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+LABEL version="1.0.2" description="Aplicacao ASP .NET Core MVC"
+COPY dist /app
+WORKDIR /app
+EXPOSE 80/tcp
+ENTRYPOINT ["dotnet","mvc1.dll"]
