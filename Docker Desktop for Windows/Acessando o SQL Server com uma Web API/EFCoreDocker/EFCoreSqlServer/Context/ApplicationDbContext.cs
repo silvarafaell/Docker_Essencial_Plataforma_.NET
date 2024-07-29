e9e024d5b353e6075a64052a4041c3259aff2f80
francisco rafael <francisco_rafael@hotmail.com.br>
2024-07-29 15:41:51 -0300

    Acessando o SQL Server com uma Web API

---
+++ Docker Desktop for Windows/Acessando o SQL Server com uma Web API/EFCoreDocker/EFCoreSqlServer/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EFCoreSqlServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSqlServer.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();  //se não existir cria o Banco
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
